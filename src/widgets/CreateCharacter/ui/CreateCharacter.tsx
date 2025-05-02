fd2199fdac14fd4aeb221bdb08cc4e8d8753b3eb
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-02 21:40:20 +0300

    added icons

--- src/widgets/CreateCharacter/ui/CreateCharacter.tsx
+++ src/widgets/CreateCharacter/ui/CreateCharacter.tsx
@@ -1,5 +1,12 @@
 import { FC, useEffect } from 'react';
-import { Accordion, Card, Text, VStack } from '@project-1114/ui-kit';
+import {
+    Accordion,
+    Card,
+    HStack,
+    Icon,
+    Text,
+    VStack,
+} from '@project-1114/ui-kit';
 import { EditSex } from '@/features/EditSex';
 import { EditFaceFeatures } from '@/features/EditFaceFeatures';
 import { EditOverlays } from '@/features/EditOverlays';
@@ -10,6 +17,14 @@ import { useInitCharacter } from '@/shared/hooks/useInitCharacter.ts';
 import { EditEyes } from '@/features/EditEyes';
 import { EditColorOverlays } from '@/features/EditColorOverlays';
 
+import EyesIcon from '../assets/eyes.svg?react';
+import FaceFeaturesIcon from '../assets/face-features.svg?react';
+import HairIcon from '../assets/hair.svg?react';
+import InheritanceIcon from '../assets/inheritance.svg?react';
+import OverlaysIcon from '../assets/overlays.svg?react';
+import SexIcon from '../assets/sex.svg?react';
+import FacialHairIcon from '../assets/facial-hair.svg?react';
+
 export const CreateCharacter: FC = () => {
     const init = useInitCharacter();
 
@@ -18,45 +33,100 @@ export const CreateCharacter: FC = () => {
     }, []);
 
     return (
-        <Card style={{width: '20%'}}>
-            <VStack gap={'xs'} max>
+        <Card style={{ width: '20%', background: 'var(--color-main)' }}>
+            <VStack gap={'l'} max>
                 <Accordion
-                    title={<Text size={'l'}>Sex</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon Svg={SexIcon} fill={'var(--color-accent)'} />
+                            <Text size={'l'}>Sex</Text>
+                        </HStack>
+                    }
                     description={<EditSex />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Inheritance</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon
+                                Svg={InheritanceIcon}
+                                fill={'var(--color-accent)'}
+                            />
+                            <Text size={'l'}>Inheritance</Text>
+                        </HStack>
+                    }
                     description={<EditInheritance />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Hair</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon Svg={HairIcon} fill={'var(--color-accent)'} />
+                            <Text size={'l'}>Hair</Text>
+                        </HStack>
+                    }
                     description={<EditHair />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Eyes</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon Svg={EyesIcon} fill={'var(--color-accent)'} />
+                            <Text size={'l'}>Eyes</Text>
+                        </HStack>
+                    }
                     description={<EditEyes />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Face Features</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon
+                                Svg={FaceFeaturesIcon}
+                                fill={'var(--color-accent)'}
+                            />
+                            <Text size={'l'}>Face features</Text>
+                        </HStack>
+                    }
                     description={<EditFaceFeatures />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Overlays</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon
+                                Svg={OverlaysIcon}
+                                fill={'var(--color-accent)'}
+                            />
+                            <Text size={'l'}>Overlays</Text>
+                        </HStack>
+                    }
                     description={<EditOverlays />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Color overlays</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon
+                                Svg={OverlaysIcon}
+                                fill={'var(--color-accent)'}
+                            />
+                            <Text size={'l'}>Color overlays</Text>
+                        </HStack>
+                    }
                     description={<EditColorOverlays />}
                     fullWidth
                 />
                 <Accordion
-                    title={<Text size={'l'}>Facial hair</Text>}
+                    title={
+                        <HStack align={'center'} gap={'l'}>
+                            <Icon
+                                Svg={FacialHairIcon}
+                                fill={'var(--color-accent)'}
+                            />
+                            <Text size={'l'}>Facial hair</Text>
+                        </HStack>
+                    }
                     description={<EditFacialHair />}
                     fullWidth
                 />
