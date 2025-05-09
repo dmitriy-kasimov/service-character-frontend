e8a49173334c89dbb10cbade95bf0245c112ec28
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-09 13:49:43 +0300

    fixed using useInitCharacter

--- src/pages/PageCreateCharacter/ui/PageCreateCharacter.tsx
+++ src/pages/PageCreateCharacter/ui/PageCreateCharacter.tsx
@@ -1,10 +1,17 @@
+import { useInitCharacter } from '@/shared/hooks/useInitCharacter';
 import { FemaleAppearance } from '@/widgets/FemaleAppearance';
 import { MainAppearance } from '@/widgets/MainAppearance';
 import { MaleAppearance } from '@/widgets/MaleAppearance';
 import { Card, HStack, VStack } from '@project-1114/ui-kit';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 export const PageCreateCharacter: FC = () => {
+    const init = useInitCharacter();
+
+    useEffect(() => {
+        init();
+    }, []);
+
     return (
         <HStack max justify={'between'} align={'start'}>
             <Card
